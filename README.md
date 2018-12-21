63e5638df95928098f5cb147fa26828870d31839
CharityQuinn <charityaquinn@gmail.com>
2018-12-20 20:18:56 -0500

    Trying to get the retrieval from the form

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # TriviaGame
 
 <!-- Greek Civilization:
-Who was Achilles?  Prince of Phitha, strongest Greek fighter
+Who was Achilles?  Prince of Phitha, Strongest Greek fighter
 Who was Patroclus? Achille's firend, killed by Hector
 Who was Argamemnon? King of Mycenae and Greek
 Who was Aphrodite? Goddess of love and beauty
@@ -9,4 +9,5 @@ Who was Hermes? God of Travelers
 Who was Apollo? God of youth, music,prophesy, archery and sun
 Who was Hephaestus God of fire
 Who was Helen? Queen of Sparta and Trojan
-Who was Priam? King of Troy -->
+Who was Priam? King of Troy 
+Who was Athena? Goddess of War-->
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,29 @@
-
-
 var cntDown;
 var start;
-var done;
+var value;
 var number = 40;
 var intervalId;
 var correct = 0;
 var incorrect = 0;
 var unanswer = 0;
-var q1;
-var q2;
-var q3;
-var q4;
-var q5;
-var q6;
-var q7;
-var q8;
-var q9;
+var rightAnswer = ["Strongest Greek fighter", "Achille's firend", "King of Mycenae", "God of Travelers", "God of youth", "God of fire", "Queen of Sparta", "King of Troy", "King of Troy", "Goddess of War"];
 
-//  When the resume button gets clicked, execute the run function.
-$("#start").on("click", run);
-$("#stop").on("click", stop);
 
+// start the clock when the Start button is clicked
+$(document).on("click", "#start", function() {
+  number = 40;
+});
+//function to run the clock
 function run() {
   clearInterval(intervalId);
   intervalId = setInterval(decrement, 1000);
 }
-
+//function to bring the timer down
 function decrement() {
   number--;
   $("#show-number").html("<h3>Time Remaining " + number + " seconds</h3>");
-  if (number === 0) {
+  
+  if (number === 0 ) {
     stop();
     alert("Time Up!");
   }
@@ -40,24 +33,50 @@ function decrement() {
 function stop() {
 
   //  Clears our intervalId
-   clearInterval(intervalId);
+  clearInterval(intervalId);
 }
 
 //  Execute the run function.
 run();
 
-function checkAnswer () {
- for (var i = 0; i< 9; i++) {
-     if (q[i] === )
- }
+
+ Function to get selected value and check it
+ //$('.cirbtn').click(function () {
+   //console.log(.cirbtn);
+   for (i = 0; i < 11; i++) {
+     value = $("input[name='q[i]']:checked").val();
+     console.log(value);
+     if (value === rightAnswer[i]) {
+       correct++;
+       console.log(value);
+       console.log(rightAnswer[i])
+     }
+          
+     else if (value = "undefined") {
+      unanswer++;
+      } 
+     else {
+     incorrect++;
+    }
+  }
+
+console.log("This is the number correct: " + correct);
+console.log("This is the value of incorrect " + incorrect);
+console.log("This is the value of unAnswered " + unanswer);
+
+  $(document).on("click", "#done", function() {
+    stop ();
+    incorrect = 0;
+    correct = 0;
+    unanswer = 0;
+
+  });
   
-}
-if $("cirbtn.name") === ("Strongest Greek fighter") {
-  correct++;
-}
-else if {
-  incorrect++;
-}
-else {
-  unanswer++;
-}
+
+
+
+
+
+//To display the selected value we used <p id="result"> tag in HTML file
+//$('.container').append(value); console.log(value);
+//})
